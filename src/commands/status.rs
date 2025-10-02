a593a7b84daf9b7fc048bcf041d5a4503dbbbde9
Josiah Morris <josiahbmorris@hotmail.com>
2025-10-03 02:58:58 +0900

    Refactored the status command to hide .nag directory, and properly show staged

--- src/commands/status.rs
+++ src/commands/status.rs
@@ -4,6 +4,7 @@ use crate::core::{
     io::read_file,
 };
 use crate::core::hash::hash;
+use crate::core::tree::read_tree_to_index;
 
 use std::fs::read_dir;
 use std::path::Path;
@@ -26,14 +27,34 @@ pub fn status() -> std::io::Result<String> {
         .collect();
     let wrk_paths: HashSet<_> = working.iter().map(|(_, p)| p.clone()).collect();
 
+    let head_path = root.join(".nag").join("HEAD");
+    let proj_head_contents = read_file(&head_path.to_string_lossy());
+    let head_str = String::from_utf8_lossy(&proj_head_contents);
+
+    let target = head_str.trim();
+    let branch_path_fragment = target.strip_prefix("ref: ").unwrap_or(target);
+    let branch_path = root.join(".nag").join(branch_path_fragment);
+    let branch_contents = read_file(&branch_path.to_string_lossy());
+    let branch_oid = String::from_utf8_lossy(&branch_contents);
+
+    let commit_path = root.join(".nag").join("objects").join(format!("{}", branch_oid));
+    let commit_contents = read_file(&commit_path.to_string_lossy());
+    let commit_str = String::from_utf8_lossy(&commit_contents);
+
+    let tree_line = commit_str.lines().collect::<Vec<&str>>()[0];
+    let tree_oid = tree_line.split(" ").collect::<Vec<&str>>()[1];
+    let head_index = read_tree_to_index(&tree_oid)?;
+
+    let head_index_map: HashMap<String, String> = head_index.into_iter()
+        .map(|(oid, p)| (p.clone(), oid.clone()))
+        .collect();
+
     for working_entry in working { 
         let wrk_oid = working_entry.0;
         let wrk_path = working_entry.1;
         if let Some(index_oid) = index_map.get(&wrk_path) {
             if &wrk_oid != index_oid {
                 modified.push(wrk_path);
-            } else {
-                staged.push(wrk_path);
             }
         } else {
             untracked.push(wrk_path);
@@ -41,9 +62,17 @@ pub fn status() -> std::io::Result<String> {
     }
 
     for index_entry in index {
+        let index_entry_oid = index_entry.0;
         let index_entry_path = index_entry.1;
         if !wrk_paths.contains(&index_entry_path) {
-            deleted.push(index_entry_path);
+            deleted.push(index_entry_path.clone());
+        }
+        if let Some(head_entry_oid) = head_index_map.get(&index_entry_path) {
+            if *head_entry_oid != index_entry_oid {
+                staged.push(index_entry_path);
+            }
+        } else {
+            staged.push(index_entry_path);
         }
     }
 
@@ -89,6 +118,9 @@ fn walk(path: &Path, working: &mut Vec<(String, String)>, root: &Path) -> std::i
     if path.is_dir() {
         for child in read_dir(path)? {
             let dir = child.unwrap();
+            if path.file_name().map_or(false, |n| n == ".nag") {
+                return Ok(());
+            }
             walk(&dir.path(), working, root)?;
         }
     } else if path.is_file() {
