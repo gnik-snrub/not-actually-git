05414b34223f74b5dd236f635f37dededd0624f8
Josiah Morris <josiahbmorris@hotmail.com>
2025-12-04 03:06:47 +0900

    Updated add_remote with repo existence validation

--- src/commands/remote.rs
+++ src/commands/remote.rs
@@ -3,9 +3,21 @@ use crate::core::refs::{
     get_ref_path,
 };
 
+use std::path::{ Path, PathBuf };
+
 pub fn add_remote(name: String, path: String) -> std::io::Result<()> {
+    let nag_path = get_remote_nag_dir(&path)?;
+    if !nag_path.exists()
+        && nag_path.join("refs/heads").exists()
+        && nag_path.join("objects").exists()
+        && nag_path.join("HEAD").exists() {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::NotFound,
+            "Not a NAG repository",
+        ));
+    }
     update_ref(&format!("refs/remotes/{}", name), &path)?;
-    Ok(())
+    return Ok(())
 }
 
 pub fn remove_remote(name: String) -> std::io::Result<()> {
@@ -18,4 +30,20 @@ pub fn remove_remote(name: String) -> std::io::Result<()> {
     }
     std::fs::remove_file(ref_path)?;
     Ok(())
+}
+
+pub fn fetch_remote(name: String) -> std::io::Result<()> {
+
+    Ok(())
+}
+
+fn get_remote_nag_dir(path: &String) -> std::io::Result<PathBuf> {
+    let nag_path = Path::new(&path).join(".nag");
+    if nag_path.is_dir() {
+        return Ok(nag_path)
+    }
+    Err(std::io::Error::new(
+        std::io::ErrorKind::NotFound,
+        "Not a NAG repository",
+    ))
 }
